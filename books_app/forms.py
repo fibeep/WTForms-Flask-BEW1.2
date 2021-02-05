19453cfcb9530d55f70371da8171cc5d5d2bd11a
Salo <salomonco@gmail.com>
2021-02-04 19:49:26 -0500

    done w/o sterchies

--- books_app/forms.py
+++ books_app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SelectField, SubmitField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
+from wtforms.fields.simple import TextAreaField
 from wtforms.validators import DataRequired, Length
 from books_app.models import Audience, Book, Author, Genre
 
@@ -17,21 +18,27 @@ class BookForm(FlaskForm):
 class AuthorForm(FlaskForm):
     """Form to create an author."""
 
-    # TODO: Fill out the fields in this class for:
+    # : Fill out the fields in this class for:
     # - the author's name
     # - the author's biography (hint: use a TextAreaField)
     # - a submit button
+    name = StringField("Author's Name", validators=[
+                       DataRequired(), Length(min=3, max=80)])
+    biography = TextAreaField("Biography", validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
     # STRETCH CHALLENGE: Add more fields here as well as in `models.py` to
     # collect more information about the author, such as their birth date,
     # country, etc.
-    pass
+    
 
 
 class GenreForm(FlaskForm):
     """Form to create a genre."""
 
-    # TODO: Fill out the fields in this class for:
     # - the genre's name (e.g. fiction, non-fiction, etc)
     # - a submit button
-    pass
+    genre = StringField("Genre:", validators=[DataRequired(), Length(min=3, max=80)])
+
+    submit = SubmitField('Submit')
+    
--- books_app/routes.py
+++ books_app/routes.py
@@ -43,27 +43,41 @@ def create_book():
 
 @main.route('/create_author', methods=['GET', 'POST'])
 def create_author():
-    # TODO: Make an AuthorForm instance
-
-    # TODO: If the form was submitted and is valid, create a new Author object
+    # : Make an AuthorForm instance
+    form = AuthorForm()
+    # : If the form was submitted and is valid, create a new Author object
     # and save to the database, then flash a success message to the user and
     # redirect to the homepage
-
-    # TODO: Send the form object to the template, and use it to render the form
+    if form.validate_on_submit():
+        new_author = Author(
+            name = form.name.data,
+            biography = form.biography.data,
+        )
+        db.session.add(new_author)
+        db.session.commit()
+        
+    # : Send the form object to the template, and use it to render the form
     # fields
-    return render_template('create_author.html')
+        return redirect(url_for('main.create_book', author=new_author))
+    return render_template('create_author.html', form=form)
 
 @main.route('/create_genre', methods=['GET', 'POST'])
 def create_genre():
-    # TODO: Make a GenreForm instance
-
-    # TODO: If the form was submitted and is valid, create a new Genre object
+    # : Make a GenreForm instance
+    form = GenreForm()
+    # : If the form was submitted and is valid, create a new Genre object
     # and save to the database, then flash a success message to the user and
     # redirect to the homepage
-
-    # TODO: Send the form object to the template, and use it to render the form
+    if form.validate_on_submit():
+        new_genre = Genre(
+            name = form.genre.data
+        )
+        db.session.add(new_genre)
+        db.session.commit()
+    # : Send the form object to the template, and use it to render the form
     # fields
-    return render_template('create_genre.html')
+        return redirect(url_for('main.create_genre', genre=new_genre))
+    return render_template('create_genre.html', form=form)
 
 @main.route('/create_user', methods=['GET', 'POST'])
 def create_user():
